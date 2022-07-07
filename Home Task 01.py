c90ef005cbdb703b35bab036f72808a019688cec
Nasrin Akter Tanya <106919412+tanya-86@users.noreply.github.com>
2022-07-07 10:50:57 +0600

    Add files via upload

---
+++ Home Task 01.py
@@ -0,0 +1,19 @@
+#Q1: Sheikh have 20 emails, He deleted 4. Then he forwarded the rest of the emails to four persons dividing equally.
+# How many emails got each of them. Design code of your own.
+
+print('The total no of remaining mails:',20 - 4)
+print('Each person got:',int(16/4), 'emails')
+
+
+#Q2: Type your Name and Birth date. Show the output which can calculate Your age.
+print('\nType your name: Nasrin Akter Tanya')
+print('Type your birth date: 20. 01. 1998')
+print("Today's date: 27. 04. 2022")
+print('Your age:',2022 - 1998)
+
+#Q3: There were 20 cyclones in the Bay of Bengal in last 5 years from September to February each year. 
+# Suppose, each month have equal cyclones each year. Calculate the total cyclone of January month for last 5 years. Design your own code.
+
+print('\nThe number of cyclones each year has:', 20/5)
+print('Each month has:', 4 / 6, 'cyclones')
+print('The total cyclones of January month for last 5 years be:', (4/6)*5)
